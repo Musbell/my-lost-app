90364007eb4d61d21cb16253bdb14a96b40a32b7
BELLO MUSA <Musbell007@gmail.com>
2018-12-19 18:57:44 +0100

    I tired

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,14 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import BlogView, BlogDetailView
+from .views import BlogView, BlogDetailView, blog
 
 
 app_name = 'blog'
 
 urlpatterns = [
         path('blog-post/', BlogView.as_view(), name='blog-post'),
-        # path('blog/', blog, name='blog_page'),
+        path('blog/', blog, name='blog_page'),
         path('blog-detail/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
 
 
